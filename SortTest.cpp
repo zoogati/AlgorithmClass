f78394cdf055a3954a536fead1cb556258b33765
Socratis Katehis <socratis.katehis@gmail.com>
2017-03-08 21:02:20 -0500

    MergeSort
    Finished the insertion sort and revised the arrayGenRev function (wasn't
    generating correctly). Currently working on mergeSort.

--- SortTest.cpp
+++ SortTest.cpp
@@ -16,11 +16,12 @@
 
 using namespace std;
 
-//Default vaules for program
+//Default values for arrays and program
 
 const int size1 = 100;
 const int size2 = 10000;
 const int size3 = 1000000;
+const int tests = 15;
 
 //Arrays with random integers
 
@@ -44,12 +45,14 @@ int arrayRev3[size3];
 
 int compareCount = 0;
 int swapCount = 0;
+int arrayCompare[tests];
+int arraySwap[tests];
 
 //Function for generating random integer array (Doesn't matter if not-unique)
     void arrayGen(int array[], int size) {
         
         for (int i = 0; i < size; i++) {
-            int element = rand() % (2*size);
+            int element = rand() % size;
             array[i] = element;
         }
     }
@@ -65,12 +68,62 @@ int swapCount = 0;
 //Function for generating array with integers n to 1
     void arrayGenRev(int array[], int size) {
         
-        for (int i = size; i < 0; i--) {
-            array[i-1] = i;
+        int lastNumber = size;
+        for (int i = 0; i < size; i++) {
+            array[i] = lastNumber;
+            lastNumber--;
         }
     }
     
 //Implementing Straight Insertion Sort
-    void insertionSort(int array[],int size) {
+    void insertionSort(int array[],int size, int testNumber) {
         
-        for (int i = 
+        for (int i = 2; i < size; i++) {
+            int key = array[i];
+            int j = i-1;
+            while (j > 0 && array[j] > key) {       //If previous entry is larger, swap it.
+                compareCount++;
+                array[j+1] = array[j];
+                swapCount++;
+                j--;
+            }
+            array[j+1] = key;
+            swapCount++;
+        }
+        arrayCompare[testNumber] = compareCount++;
+        arraySwap[testNumber] = swapCount++;
+    }
+    
+//Implementing Merge Sort (Recursive function call found in main)
+    void merge(int array[], int size, int low, int mid, int high, int testNumber) {
+        int leftIndex = mid - low - 1;
+        int rightIndex = high - mid;
+        int leftArray[(size/2)+1];          //Hold left half indices plus an end point (hence size / 2)
+        for (int i = 0; i < leftIndex+3; i++) {
+            leftArray[i] = i+1;
+        }
+        int rightArray[(size/2)+1];         //Hold right half indices
+        for (int i = 0; i < rightIndex+2; i++) {
+            rightArray[i] = i+1;
+        }
+    }
+    
+    int main() {
+        
+        arrayGenFwd(arrayFwd1, size1);
+        arrayGenFwd(arrayFwd2, size2);
+        arrayGenFwd(arrayFwd3, size3);
+        arrayGenRev(arrayRev1, size1);
+        arrayGenRev(arrayRev2, size2);
+        arrayGenRev(arrayRev3, size3);
+        
+        /*
+        for (int i = 0; i < size1; i++) {       Testing Array Generation
+            cout << arrayFwd1[i] << " ";
+        }
+        cout << endl;
+        for (int i = 0; i < size1; i++) {
+            cout << arrayRev1[i] << " ";
+        }
+        */
+    }
